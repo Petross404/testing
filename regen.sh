faef9c8e6345fbf3bb20425ebcfd375b539eeaf1
Petross404 <petross404@gmail.com>
2017-02-02 15:04:52 +0200

    add openra

--- regen.sh
+++ regen.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-echo "masters = gentoo" > ./metadata/layout.conf && echo "cache-formats = md5-dict" >> ./metadata/layout.conf || exit -2;
-touch ./profiles/repo_name && echo "testing" > ./profiles/repo_name || exit -3;
+echo "masters = gentoo" > /var/lib/layman/testing/metadata/layout.conf && echo "cache-formats = md5-dict" >> /var/lib/layman/testing/metadata/layout.conf || exit -2;
+touch /var/lib/layman/testing/profiles/repo_name && echo "testing" > /var/lib/layman/testing/profiles/repo_name || exit -3;
 repoman manifest && emerge --regen 
 emerge --metadata
 repoman --digest=y -d full 
-egencache --jobs=2 --update --repo local
+egencache --jobs=2 --update --repo testing
